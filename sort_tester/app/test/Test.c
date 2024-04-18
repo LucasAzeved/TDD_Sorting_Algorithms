a3ecffbbc87fbb86bebfe22a550ac189f2ac4fe9
Lucas Azevedo <lucasc.azevedo@hotmail.com>
2024-04-18 19:15:38 -0300

    ALeR

--- sort_tester/app/test/Test.c
+++ sort_tester/app/test/Test.c
@@ -1,4 +1,4 @@
-#include "foo.h"
+#include "sort.h"
 #include "unity.h"
 #include "unity_fixture.h"
 
@@ -14,15 +14,16 @@ TEST_TEAR_DOWN(Foo)
 
 TEST(Foo, TestFoo1)
 {
-  // All of these should pass
-  TEST_ASSERT_EQUAL(-2, foo(1,2));
-  TEST_ASSERT_EQUAL(-1, foo(2,2));
+    int a[] = {1, 2, 3, 5, 4};
+    int length = 5;
+    const char* type = "On2"; // 0 e 1 = On, 2 a 4 = On2, 5 a 7 = Onlogn
+    int algorithm = 3; // 0 e 1 = On, 2 a 4 = On2, 5 a 7 = Onlogn
+    
+    TEST_ASSERT_EQUAL(0, sort(a, length, (char *)type, algorithm));
 }
 
 TEST(Foo, TestFoo2)
 {
   // This test will fail
-  //TEST_ASSERT_EQUAL(2, foo(1,2));
-  // This test will pass
-  TEST_ASSERT_EQUAL(-1, foo(2,2));
+  // TEST_ASSERT_EQUAL(2, foo(1,2));
 }
